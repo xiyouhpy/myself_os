2cdf11cabf6d4a192f997432a4a115cbf5ffe878
xiyouhpy <xiyouhanpeiyan@gmail.com>
2014-03-16 23:04:12 +0800

    内核启动入口程序

---
+++ src/init/hpy_main.c
@@ -0,0 +1,22 @@
+/*************************************************************************
+    > File Name: hpy_main.c
+    > Author: 韩佩言
+    > Mail: xiyouhanpeiyan@gmail.com 
+    > Created Time: 2013年11月18日 星期一 15时01分36秒
+    > Class: 计科1107
+ ************************************************************************/
+// 内核入口函数
+
+#include "multiboot.h"
+#include "common.h"
+#include "monitor.h"
+#include "types.h"
+
+int hpy_main()
+{
+	monitor_clear();
+	monitor_write("hello world!\n");
+	
+	return 0;
+}
+
